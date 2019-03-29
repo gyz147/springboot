561f6bf0e0bb220aa40a22f1add7a624c9ca4044
gyz147 <18360922022@163.com>
2019-03-29 10:15:36 +0800

    Spring Boot集成JPA框架

--- springboot05-JPAValidate/src/main/java/com/gyz/springboot/controller/UserController.java
+++ springboot05-JPAValidate/src/main/java/com/gyz/springboot/controller/UserController.java
@@ -15,8 +15,6 @@ import javax.validation.Valid;
 
 /**
  * 用户控制层
- *
- * Created by bysocket on 24/07/2017.
  */
 @Controller
 @RequestMapping(value = "/users")     // 通过这里配置使下面的映射都在 /users
@@ -26,9 +24,9 @@ public class UserController {
     UserService userService;          // 用户服务层
 
     /**
-     *  获取用户列表
-     *    处理 "/users" 的 GET 请求，用来获取用户列表
-     *    通过 @RequestParam 传递参数，进一步实现条件查询或者分页查询
+     * 获取用户列表
+     * 处理 "/users" 的 GET 请求，用来获取用户列表
+     * 通过 @RequestParam 传递参数，进一步实现条件查询或者分页查询
      */
     @RequestMapping(method = RequestMethod.GET)
     public String getUserList(ModelMap map) {
@@ -50,9 +48,9 @@ public class UserController {
     }
 
     /**
-     *  创建用户
-     *    处理 "/users" 的 POST 请求，用来获取用户列表
-     *    通过 @ModelAttribute 绑定参数，也通过 @RequestParam 从页面中传递参数
+     * 创建用户
+     * 处理 "/users" 的 POST 请求，用来获取用户列表
+     * 通过 @ModelAttribute 绑定参数，也通过 @RequestParam 从页面中传递参数
      */
     @RequestMapping(value = "/create", method = RequestMethod.POST)
     public String postUser(ModelMap map,
@@ -72,11 +70,11 @@ public class UserController {
 
     /**
      * 显示需要更新用户表单
-     *    处理 "/users/{id}" 的 GET 请求，通过 URL 中的 id 值获取 User 信息
-     *    URL 中的 id ，通过 @PathVariable 绑定参数
+     * 处理 "/users/{id}" 的 GET 请求，通过 URL 中的 id 值获取 User 信息
+     * URL 中的 id ，通过 @PathVariable 绑定参数
      */
     @RequestMapping(value = "/update/{id}", method = RequestMethod.GET)
-    public String getUser(@PathVariable Long id, ModelMap map) {
+    public String getUser(ModelMap map, @PathVariable Long id) {
         map.addAttribute("user", userService.findById(id));
         map.addAttribute("action", "update");
         return "userForm";
@@ -84,7 +82,6 @@ public class UserController {
 
     /**
      * 处理 "/users/{id}" 的 PUT 请求，用来更新 User 信息
-     *
      */
     @RequestMapping(value = "/update", method = RequestMethod.POST)
     public String putUser(ModelMap map,
